c7119ec0a493cfaf603fe3a89e9a31d38ba71c66
OmitNomis <simon98190@gmail.com>
2024-06-30 16:02:21 +0545

    add settings modal

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
-console.log("script working");
+const settingsBtn = document.getElementById("settingsBtn");
+const settingsModal = document.getElementById("settingsModal");
+const closeBtn = document.getElementsByClassName("close")[0];
+
+settingsBtn.addEventListener("click", () => {
+  settingsModal.style.display = "block";
+});
+closeBtn.addEventListener("click", () => {
+  settingsModal.style.display = "none";
+});
